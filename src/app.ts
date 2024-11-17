d23e72a37b9fa4b3cfa8d2126c0cd0fe08cd1457
Dariusz Filipiak <darekf77@gmail.com>
2024-11-17 07:59:23 +0100

    release:  v18.0.13

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,27 @@
 //#region @notForNpm
-import { Taon } from 'taon'
+import { Taon } from 'taon';
 
 export async function start() {
-
-  console.log('hello')
-
+  console.log('hello');
 }
 
 export default start;
+//#endregion
+
+//#region  record-replay-req-res-scenario component
+//#region @browser
+@Component({ template: 'hello world fromr record-replay-req-res-scenario' })
+export class RecordReplayReqResScenarioComponent {}
+//#endregion
+//#endregion
+
+//#region  record-replay-req-res-scenario module
+//#region @browser
+@NgModule({
+  declarations: [RecordReplayReqResScenarioComponent],
+  imports: [CommonModule],
+  exports: [RecordReplayReqResScenarioComponent],
+})
+export class RecordReplayReqResScenarioModule {}
+//#endregion
 //#endregion
