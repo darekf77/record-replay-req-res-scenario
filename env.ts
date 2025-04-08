8c3d9d8149e5c27a309779a3f0b97967fc4ab25c
Dariusz Filipiak <darekf77@gmail.com>
2025-04-08 20:26:58 +0200

    release: v18.0.20

---
+++ env.ts
@@ -0,0 +1,9 @@
+import type { EnvOptions } from 'tnp/src';
+
+const env: Partial<EnvOptions> = {
+  website: {
+    domain: 'record-replay-req-res-scenario.example.domain.com',
+    title: 'Record Replay Req Res Scenario',
+  },
+};
+export default env;
